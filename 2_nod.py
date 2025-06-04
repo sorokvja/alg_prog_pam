64c2acf66929816cd367229f3e3ba90567988d61
sorokvja <vjacheslav.sorokin@gmail.com>
2025-06-04 21:30:32 +0300

    2.5 tasks solved

--- 2_nod.py
+++ 2_nod.py
@@ -100,4 +100,33 @@ def main():
 main()
 
 #2.5 Uzdevuma kods
+def get_variables():
+    while True:
+        try:
+            nat_int = int(input('Ievadiet naturālo skaitli ==> '))
+            if nat_int < 0:
+                print("Kļūda #1: ievadītais skaitlis neesot naturāls skaitlis!")
+                continue
+            return nat_int
+        except ValueError:
+            print("Kļūda #2: ievadīta vērtība neesot naturāls skaitlis!")
+
+def do_action_1(a):
+    return a % 10
+
+def do_action_2(a):
+    while a >= 10:
+        a -= 10
+    return a
+
+def main():
+    a = get_variables()
+    b_1 = do_action_1(a)
+    print(f"1. metode: skaitļa {a} pedējais cipars ir {b_1}")
+    b_2 = do_action_2(a)
+    print(f"2. metode: skaitļa {a} pedējais cipars ir {b_2}")
+
+main()
+
+#2.6 Uzdevuma kods
 
--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 def get_variables():
-    int_eur = int(input('Eiro ==> '))
-    int_cent = int(input('Centi ==> '))
-    int_cookies = int(input('Kūku skaits ==> '))
-    return int_eur, int_cent, int_cookies
+    while True:
+        try:
+            nat_int = int(input('Ievadiet naturālo skaitli ==> '))
+            if nat_int < 0:
+                print("Kļūda #1: ievadītais skaitlis neesot naturāls skaitlis!")
+                continue
+            return nat_int
+        except ValueError:
+            print("Kļūda #2: ievadīta vērtība neesot naturāls skaitlis!")
 
-def do_action(a, b, n):
-    eur = a * n
-    cents = b * n
-    #print(f"eur_a: {eur}, cents_b: {cents}")
-    eur += cents // 100
-    cents %= 100
-    print(f"Par {n} kūkam ir jāmaksā {eur} Eiro un {cents} centi")
+def do_action_1(a):
+    return a % 10
+
+def do_action_2(a):
+    while a >= 10:
+        a -= 10
+    return a
 
 def main():
-    print("Ievadiet vienas kūkas cenu un kūku skaitu:")
-    a, b, n = get_variables()
-    do_action(a, b, n)
+    a = get_variables()
+    b_1 = do_action_1(a)
+    print(f"1. metode: skaitļa {a} pedējais cipars ir {b_1}")
+    b_2 = do_action_2(a)
+    print(f"2. metode: skaitļa {a} pedējais cipars ir {b_2}")
 
 main()
