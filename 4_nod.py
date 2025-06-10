360af9c9025694cfbd7cbf016995c33718843f1e
sorokvja <vjacheslav.sorokin@gmail.com>
2025-06-11 00:09:58 +0300

    solve 7.1. task, create 4_nod.py file

---
+++ 4_nod.py
@@ -0,0 +1,1 @@
+# 4.1. Uzdevuma kods
---
+++ 7_nod.py
@@ -0,0 +1,29 @@
+# 7.1. Uzdevuma kods
+def get_input():
+    entered_text = input('Ievadiet tekstu ==> ')
+    return entered_text
+
+def do_action(the_text):
+    reversed_text = the_text[::-1]
+    if the_text[0].isupper():
+        reversed_text = reversed_text.capitalize()
+    print(f"Apgriezts teksts: {reversed_text}")
+
+def do_action_alt(the_text):
+    ol_reversed_text = the_text[::-1] # original letters
+    reversed_text = ""
+    for i in range (len(the_text)):
+        if the_text[i].isupper():
+            reversed_text += ol_reversed_text[i].upper()
+        else:
+            reversed_text += ol_reversed_text[i].lower()
+    print(f"Apgriezts teksts (alternatīvs risinājums): {reversed_text}")
+
+def main():
+    teksts = get_input()
+    do_action(teksts)
+    do_action_alt(teksts)
+
+main()
+
+# 7.2. Uzdevuma kods
