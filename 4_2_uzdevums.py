45ac2f1722475af9c989d4a1a922b2e526c55570
sorokvja <vjacheslav.sorokin@gmail.com>
2025-06-17 18:08:11 +0300

    4_2_uzdevums - add second task

--- 4_2_uzdevums.py
+++ 4_2_uzdevums.py
@@ -18,7 +18,6 @@ def dice_roll(dice_min_val, dice_max_val, rolls):
 def two_player_game():
     rolls_to_make = 6
     players_scores = {"A": 0, "B": 0}
-    winner = []
     for player in players_scores:
         print(f"Spēlētājs {player} met kauliņu:")
         roll_list = random.choices(range(1, 7), k=rolls_to_make)
@@ -26,6 +25,21 @@ def two_player_game():
             print(f"{count}. metiens: {item}")
         players_scores[player] = sum(roll_list)
         print(f"Kopā punkti spēlētājam {player}: {players_scores[player]}\n")
+    return players_scores
+
+def hundred_points_game():
+    print("Spēles mērķis: pirmajam sasniegt vismaz 100 punktus.\n")
+    players_scores = {"User": 0, "PC": 0}
+    winner = []
+    while players_scores["User"] < 100 and players_scores["PC"] < 100:
+        user_rolls = random.choices(range(1, 7), k=2)
+        players_scores["User"] += sum(user_rolls)
+        print(f'Lietotājs | {user_rolls[0]}  {user_rolls[1]} | {players_scores["User"]}')
+        pc_rolls = random.choices(range(1, 7), k=2)
+        players_scores["PC"] += sum(pc_rolls)
+        print(f'Dators    | {pc_rolls[0]}  {pc_rolls[1]} | {players_scores["PC"]}\n')
+    print("Spēle beigusies!")
+    return players_scores
     max_score = max(players_scores.values())
     for player, score in players_scores.items():
         if score == max_score:
@@ -33,18 +47,32 @@ def two_player_game():
     if len(winner) > 1:
         print("Vairākiem spēlētājiem ir vienāds rezultāts, mēģiniet vēlreiz!")
     else:
-        print(f"Uzvar spēlētājs {winner[0]}")
+        print(f'{winner[0]} uzvarēja!\nIegūtie punkti:')
+        print(f'')
 
-def hundred_points_game():
-    pass
+def the_winner(players_scores):
+    winner = []
+    max_score = max(players_scores.values())
+    for player, score in players_scores.items():
+        if score == max_score:
+            winner.append(player)
+    if len(winner) > 1:
+        print("Vairākiem spēlētājiem ir vienāds rezultāts, mēģiniet vēlreiz!")
+    else:
+        print(f'{winner[0]} uzvarēja!\nIegūtie punkti:')
+        i = 0
+        for player, score in players_scores.items():
+            print(f'{player}: {score}')
 
 def main():
     user_input = get_input()
     if user_input == "Q":
         print("Lietotājs izvēlējas pabeigt spēli.\n")
     elif user_input == "1":
-        two_player_game()
+        players_and_scores = two_player_game()
+        the_winner(players_and_scores)
     else:
-        hundred_points_game()
+        players_and_scores = hundred_points_game()
+        the_winner(players_and_scores)
 
 main()
